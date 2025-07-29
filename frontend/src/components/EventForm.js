b11079a914d184e6591c5a35013f277e332a33a2
zkrakus <zkrakus@gmail.com>
2025-07-29 13:57:37 -0400

    feat:s23-l392 Added JWT Bearer token as part of fetch api calls.

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
+import { getAuthToken } from '../util/auth';
 
 function EventForm({ method, event }) {
   const data = useActionData();
@@ -86,7 +87,7 @@ export default EventForm;
 export async function action({ request, params }) {
   const method = request.method;
   const data = await request.formData();
-
+  
   const eventData = {
     title: data.get('title'),
     image: data.get('image'),
@@ -101,10 +102,12 @@ export async function action({ request, params }) {
     url = 'http://localhost:8080/events/' + eventId;
   }
 
+  const token = getAuthToken();
   const response = await fetch(url, {
     method: method,
     headers: {
       'Content-Type': 'application/json',
+      'Authorization' : 'Bearer ' + token,
     },
     body: JSON.stringify(eventData),
   });
